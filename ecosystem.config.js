2c8849fd9d351ae998cc56a3ddb71ff0fede4517
wlgns1501 <wlgns1501@gmail.com>
2023-06-12 16:20:17 +0900

    feture baseentity생성_20230612

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
         DB_PORT: process.env.DB_PORT,
         DB_USERNAME: process.env.DB_USERNAME,
         DB_PASSWORD: process.env.DB_PASSWORD,
-        DB_DATABASE: process.env.DB_DATABASE,
+        DB_NAME: process.env.DB_NAME,
       },
     },
   ],
