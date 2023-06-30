bcf2636237293c01959ce814e9610950092184a3
wlgns1501 <wlgns1501@gmail.com>
2023-06-30 15:05:25 +0900

    feture swagger setting_20230630

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('WeatherBly')
+    .setDescription('WeatherBly API Description')
+    .setVersion('3.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        in: 'header',
+        scheme: 'bearer',
+        name: 'JWT',
+        bearerFormat: 'JWT',
+      },
+      'authorization',
+    )
+    .build();
+
+  const swaggerCustomOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  };
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, swaggerCustomOptions);
 
   await app.listen(3000);
 }
