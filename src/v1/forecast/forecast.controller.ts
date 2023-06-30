128e05aa03fc183f9b69225b43c883c0fed070ac
BaeJaeWon <hohre12@gmail.com>
2023-06-30 18:09:23 +0900

    ADD] 구/읍(면)을 기준으로 데이터 캐싱 설정

--- src/v1/forecast/forecast.controller.ts
+++ src/v1/forecast/forecast.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Inject, Controller, Get, Query } from '@nestjs/common';
 import { ForecastService } from './forecast.service';
-
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { calculateMS } from '../../lib/utils/calculate';
 @Controller('forecast')
 export class ForecastController {
-  constructor(private readonly forecastService: ForecastService) {}
+  constructor(
+    private readonly forecastService: ForecastService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   @Get('getUltraSrtNcst')
   async getUltraSrtNcst() {
@@ -12,12 +17,34 @@ export class ForecastController {
   }
 
   @Get('getUltraSrtFcst')
-  async getUltraSrtFcst(@Query('x') x: string, @Query('y') y: string) {
-    const data = await this.forecastService.getUltraSrtFcst(
-      parseFloat(x),
-      parseFloat(y),
+  async getUltraSrtFcst(
+    @Query('location') location: string,
+    @Query('x') x: string,
+    @Query('y') y: string,
+  ) {
+    // 같은구에 조회가 들어올때
+    const cacheData: any | null = await this.cacheManager.get(
+      `UltraSrtFcst_${location}`,
     );
-    return data.items.item;
+    if (cacheData) {
+      return cacheData;
+    } else {
+      const data = await this.forecastService.getUltraSrtFcst(
+        parseFloat(x),
+        parseFloat(y),
+      );
+
+      if (location) {
+        const milliSeconds = calculateMS(45);
+        await this.cacheManager.set(
+          `UltraSrtFcst_${location}`,
+          data.items.item,
+          milliSeconds,
+        );
+      }
+
+      return data.items.item;
+    }
   }
 
   @Get('getOpenForecastMidInfo')
--- src/v1/forecast/forecast.service.ts
+++ src/v1/forecast/forecast.service.ts
@@ -15,22 +15,6 @@ export class ForecastService {
     this.axiosInstance = createPublicApiAxiosInstance();
   }
 
-  /* 초단기실황 : 안쓰이나? */
-  async getUltraSrtNcst() {
-    const response = await this.axiosInstance.get(
-      `/VilageFcstInfoService_2.0/getUltraSrtNcst`,
-      {
-        params: {
-          ...getBaseDateTime(),
-          nx: 55, // 계산
-          ny: 127, // 계산
-        },
-      },
-    );
-
-    return response.data.response?.body;
-  }
-
   /* 초단기예보 : 메인
   T1H - 기온 - 현재기온 : 섭씨온도
   RN1 - 1시간 강수량 - 현재 비오는지 and 강수량 : mm
@@ -44,9 +28,8 @@ export class ForecastService {
   SKY - 하늘상태 - 코드값 : 맑음(1) , 구름많음(3) , 흐림(4)
   LGT - 낙뢰 
   */
+  // test : 서울 성북구 위도 경도 - 37.58638333 , 127.0203333
   async getUltraSrtFcst(x, y) {
-    // test : 서울 성북구 위도 경도 - 37.58638333 , 127.0203333
-    console.log('xy!!!', x, y);
     const xyObj = dfsXyConvert('TO_GRID', x, y);
     const response = await this.axiosInstance.get(
       `/VilageFcstInfoService_2.0/getUltraSrtFcst`,
@@ -88,6 +71,22 @@ export class ForecastService {
         params: {
           regId: '11B10101',
           tmFc: '202306290600',
+        },
+      },
+    );
+
+    return response.data.response?.body;
+  }
+
+  /* 초단기실황 : 안쓰이나? */
+  async getUltraSrtNcst() {
+    const response = await this.axiosInstance.get(
+      `/VilageFcstInfoService_2.0/getUltraSrtNcst`,
+      {
+        params: {
+          ...getBaseDateTime(),
+          nx: 55, // 계산
+          ny: 127, // 계산
         },
       },
     );
