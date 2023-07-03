3e5fca5d228e8b8ecb97ce214b21fd47412fa26c
BaeJaeWon <hohre12@gmail.com>
2023-07-03 18:11:40 +0900

    ADD] 기상청 api 로직 수정중...

--- src/v1/forecast/forecast.controller.ts
+++ src/v1/forecast/forecast.controller.ts
@@ -5,15 +5,26 @@ import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { calculateMS } from '../../lib/utils/calculate';
 @Controller('forecast')
 export class ForecastController {
+  // 메인 : 현재온도 , 바람 , 날씨 -> 단기예보 -> 초단기예보 ( getUltraSrtFcst() )
+  // // 온보딩 : 위치 기준 어제 , 그저께 최저온도 , 최고온도 ( getWthrDataList() )
+  // 메인 : 오늘 최저온도 , 최고온도 response => 단기예보 -> 단기예보 ( getUltraSrtFcst() )
+  // 메인 -> 주간 예보 : 오늘로부터 최대 10일간의 최저온도 최고온도 제공 => 중기예보 -> 중기기온조회( getOpenForecastMidInfo() )
   constructor(
     private readonly forecastService: ForecastService,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
-  @Get('getUltraSrtNcst')
-  async getUltraSrtNcst() {
-    const data = await this.forecastService.getUltraSrtNcst();
-    return data.items.item;
+  // 온보딩 - 체감온도 설정시
+  @Get('getWthrDataList')
+  async getWthrDataList() {
+    const data = await this.forecastService.getWthrDataList();
+    return data;
+  }
+
+  @Get('getVilageFcst')
+  async getVilageFcst() {
+    const data = await this.forecastService.getVilageFcst();
+    return data;
   }
 
   @Get('getUltraSrtFcst')
@@ -22,12 +33,29 @@ export class ForecastController {
     @Query('x') x: string,
     @Query('y') y: string,
   ): Promise<any> {
+    /*  
+    ! Question - 구(읍/면) 단위로 조회할지, 동 단위로 조회할지?
+      - 구(읍/면) 단위로 조회시
+        1. 동 보다 범위가 커서 api 콜횟수를 줄일수있음.
+        2. 유저 온보딩시 동네단위로 지역설정을 하기때문에 구(읍/면)로 조회하는게 괜찮은가 의문.
+        3. 동별로 날씨차이가 크게 없기때문에 구(읍/면) 단위로 조회해도 괜찮지않을까..? 
+    */
+    /* 
+    flow 흐름 - start
+    1. address_id를 받는다
+    2. address 테이블 findOne
+    3. gu or dong : location
+    4. x - 위도 , y - 경도
+    5. 캐시 조회
+    6. 없으면 api 조회
+    7. response
+    - end 
+    */
     // 같은구에 조회가 들어올때
     const cacheData: any | null = await this.cacheManager.get(
       `UltraSrtFcst_${location}`,
     );
 
-    console.log(cacheData);
     if (cacheData) {
       return cacheData;
     } else {
@@ -52,6 +80,6 @@ export class ForecastController {
   @Get('getOpenForecastMidInfo')
   async getOpenForecastMidInfo() {
     const data = await this.forecastService.getOpenForecastMidInfo();
-    return data.items.item;
+    return data;
   }
 }
--- src/v1/forecast/forecast.service.ts
+++ src/v1/forecast/forecast.service.ts
@@ -15,6 +15,82 @@ export class ForecastService {
     this.axiosInstance = createPublicApiAxiosInstance();
   }
 
+  // 온보딩 : 위치 기준 -  어제 , 그저께 최저온도 , 최고온도
+  async getWthrDataList() {
+    // address_id 받아야됨
+
+    const beforeYesterday = getBaseDateTime({ provide: 2880 }).base_date;
+    const yesterday = getBaseDateTime({ provide: 1440 }).base_date;
+
+    const response = await this.axiosInstance.get(
+      `/AsosDalyInfoService/getWthrDataList`,
+      {
+        params: {
+          dataCd: 'ASOS', // 자료 코드
+          dateCd: 'DAY', // 날짜 코드
+          startDt: beforeYesterday, // 시작일
+          endDt: yesterday, // 종료일
+          stnIds: 108, // 지점 번호 - 서울
+        },
+      },
+    );
+
+    const data =
+      response.data.response.body?.items?.item.map((it) => {
+        return {
+          date: it.tm,
+          minTmp: it.minTa,
+          maxTmp: it.maxTa,
+        };
+      }) ?? [];
+    return data;
+  }
+
+  // 메인 : 오늘 최저온도 , 최고온도 response => 단기예보 -> 단기예보
+  async getVilageFcst() {
+    const yesterday = getBaseDateTime({ provide: 1440 });
+    const today = getBaseDateTime({ provide: 0 });
+
+    const response = await this.axiosInstance.get(
+      `/VilageFcstInfoService_2.0/getVilageFcst`,
+      {
+        params: {
+          base_date: yesterday.base_date,
+          base_time: '2300',
+          nx: 55, // 계산
+          ny: 127, // 계산
+        },
+      },
+    );
+
+    const data =
+      response.data.response.body?.items?.item
+        .filter(
+          (it) =>
+            it.fcstDate === today.base_date &&
+            (it.category === 'TMN' || // 최고기온
+              it.category === 'TMX'), // 최저기온
+          // it.category === 'SKY' || // 하늘상태
+          // it.category === 'TMP', // 1시간 체감온도
+          // it.category === 'PCP' || // 1시간 강수량
+          // it.category === 'REH' || // 습도
+          // it.category === 'PTY' || // 강수형태
+          // it.category === 'WSD', // 풍속
+        )
+        .map((it) => {
+          return {
+            category: it.category,
+            date: it.fcstDate,
+            time: it.fcstTime,
+            tmp: it.fcstValue,
+          };
+        }) ?? [];
+
+    console.log(data);
+
+    return data;
+  }
+
   /* 초단기예보 : 메인
   T1H - 기온 - 현재기온 : 섭씨온도
   RN1 - 1시간 강수량 - 현재 비오는지 and 강수량 : mm
@@ -29,6 +105,8 @@ export class ForecastService {
   LGT - 낙뢰 
   */
   // test : 서울 성북구 위도 경도 - 37.58638333 , 127.0203333
+
+  // 메인 : 현재온도 , 바람 , 날씨 -> 단기예보 -> 초단기예보
   async getUltraSrtFcst<T>(x: number, y: number): Promise<T> {
     const xyObj = dfsXyConvert('TO_GRID', x, y);
     const response = await this.axiosInstance.get(
@@ -44,28 +122,30 @@ export class ForecastService {
 
     const nowHours = getBaseDateTime({ provide: 0 }).base_time;
 
-    const data = response.data.response.body.items.item
-      .filter(
-        (it) =>
-          it.fcstTime === nowHours &&
-          (it.category === 'SKY' ||
-            it.category === 'T1H' ||
-            it.category === 'RN1' ||
-            it.category === 'REH' ||
-            it.category === 'PTY' ||
-            it.category === 'WSD'),
-      )
-      .map((it) => {
-        return {
-          category: it.category,
-          value: it.fcstValue,
-          dateTime: it.fcstTime,
-        };
-      });
-    console.log(data);
+    const data =
+      response.data.response.body?.items?.item
+        .filter(
+          (it) =>
+            it.fcstTime === nowHours &&
+            (it.category === 'SKY' ||
+              it.category === 'T1H' ||
+              it.category === 'RN1' ||
+              it.category === 'REH' ||
+              it.category === 'PTY' ||
+              it.category === 'WSD'),
+        )
+        .map((it) => {
+          return {
+            category: it.category,
+            value: it.fcstValue,
+            dateTime: it.fcstTime,
+          };
+        }) ?? [];
+
     return data;
   }
 
+  // 메인 -> 주간 예보 : 오늘로부터 최대 10일간의 최저온도 최고온도 제공 => 중기예보 -> 중기기온조회(getMidTa)
   // 중기예보 : 온보딩 - 최저 , 최고기온 || 10일간 예보 화면
   async getOpenForecastMidInfo() {
     const response = await this.axiosInstance.get(
@@ -73,27 +153,14 @@ export class ForecastService {
       {
         params: {
           regId: '11B10101',
-          tmFc: '202306290600',
+          tmFc: '202307031800',
         },
       },
     );
 
-    return response.data.response?.body;
-  }
+    console.log(response.data.response);
+    const data = response.data.response.body?.items?.item ?? [];
 
-  /* 초단기실황 : 안쓰이나? */
-  async getUltraSrtNcst() {
-    const response = await this.axiosInstance.get(
-      `/VilageFcstInfoService_2.0/getUltraSrtNcst`,
-      {
-        params: {
-          ...getBaseDateTime(),
-          nx: 55, // 계산
-          ny: 127, // 계산
-        },
-      },
-    );
-
-    return response.data.response?.body;
+    return data;
   }
 }
