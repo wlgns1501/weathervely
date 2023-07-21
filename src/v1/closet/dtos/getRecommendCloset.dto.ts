5ad659bc9d2b5eedb1b07b1e94e10a932b80f176
BaeJaeWon <hohre12@gmail.com>
2023-07-21 17:30:16 +0900

    Update getRecommendCloset.dto.ts

--- src/v1/closet/dtos/getRecommendCloset.dto.ts
+++ src/v1/closet/dtos/getRecommendCloset.dto.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class GetRecommendClosetDto {
-  @ApiProperty({ description: 'ex) 2023-07-10 23:00' })
+  @ApiProperty({
+    description:
+      'API 콜 기준 2일전까지만 기상청 api 데이터 제공하므로 테스트시 어제 or 오늘 (미래는 안됨) 정각시간으로 보내면됨 - ex) 2023-07-20 23:00',
+  })
   dateTime: string;
 }
