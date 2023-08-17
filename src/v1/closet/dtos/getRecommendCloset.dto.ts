b31e0cb07355287822cebf09ee18f79fd0d2a436
BaeJaeWon <hohre12@gmail.com>
2023-08-17 19:51:07 +0900

    ADD] 온도범위 수정 및 typeorm 쿼리 수정 , description , example 수정

--- src/v1/closet/dtos/getRecommendCloset.dto.ts
+++ src/v1/closet/dtos/getRecommendCloset.dto.ts
@@ -2,8 +2,8 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class GetRecommendClosetDto {
   @ApiProperty({
-    description:
-      'API 콜 기준 오늘 자정 ~ 내일 23:00 시로 보내면됨 - ex) 2023-07-20 23:00',
+    example: '2023-08-17 23:00',
+    description: '조회시간',
   })
   dateTime: string;
 }
