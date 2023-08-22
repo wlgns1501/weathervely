e3b30181bd0a03e71cfab085dd099658acbc3c40
wlgns1501 <wlgns1501@gmail.com>
2023-08-22 15:44:36 +0900

    edit] 20230822 update nickname

--- src/v1/user/dtos/updateUserNickNameGender.pipe.ts
+++ src/v1/user/dtos/updateUserNickNameGender.pipe.ts
@@ -9,6 +9,7 @@ import { UpdateUserNickNameGenderDto } from './updateUserNickNameGender.dto';
 import * as Joi from 'joi';
 import { SCHEMA } from 'src/lib/constant/schema';
 import { HTTP_ERROR } from 'src/lib/constant/httpError';
+import { NICKNAME_REGEX } from 'src/lib/constant/regex';
 
 @Injectable()
 export class UpdateUserNickNameGenderPipe
@@ -16,7 +17,7 @@ export class UpdateUserNickNameGenderPipe
 {
   transform(value: UpdateUserNickNameGenderDto) {
     const validationSchema = Joi.object({
-      nickname: SCHEMA.REQUIRED_STRING('nickname'),
+      nickname: SCHEMA.REQUIRED_STRING_WITH_REGEX('닉네임', NICKNAME_REGEX),
     });
 
     const { error, value: validatedValue } = validationSchema.validate(value);
@@ -25,11 +26,20 @@ export class UpdateUserNickNameGenderPipe
       throw new HttpException(
         {
           message: HTTP_ERROR.VALIDATED_ERROR,
-          detail: error.details[0].context.label,
+          detail: '닉네임에는 띄어쓰기, 쉼표를 사용할 수 없습니다.',
         },
         HttpStatus.BAD_REQUEST,
       );
     }
+
+    if (validatedValue.nickname.length >= 10)
+      throw new HttpException(
+        {
+          message: HTTP_ERROR.VALIDATED_ERROR,
+          detail: '닉네임은 10글자 이상이 될 수 없습니다.',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
 
     return validatedValue;
   }
