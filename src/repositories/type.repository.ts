b50b7651a72452f80cebc2e81214c3cb8e822d6e
wlgns1501 <wlgns1501@gmail.com>
2023-08-24 16:10:05 +0900

    feature] 20230824 세로데이터 타입 2개 가져와서 일치하는 것들

--- src/repositories/type.repository.ts
+++ src/repositories/type.repository.ts
@@ -15,8 +15,50 @@ export class TypeRepository extends Repository<Type> {
 
   async getTypeId(closet_id: Closet | number) {
     return await this.createQueryBuilder('t')
+      .select('group_concat(t.id) as type_ids')
       .leftJoin('closet_type', 'ct', 'ct.type_id = t.id')
       .where('ct.closet_id = :closetId', { closetId: closet_id })
-      .getOne();
+      .getRawOne();
+  }
+
+  // async fromTypeQuery() {
+  //   return await this.createQueryBuilder('t')
+  //     .select('DISTINCT(t.id) as type_id')
+  //     .orderBy('rand()')
+  //     .limit(2)
+  //     .getQuery();
+  // }
+
+  async typeQuery() {
+    const fromTypeQuery1 = `
+      
+        SELECT
+          t.id as type_id
+        FROM 
+          type t 
+        where
+          t.id in (1,2)
+        ORDER BY rand() ASC 
+        LIMIT 1
+      `;
+
+    const fromTypeQuery2 = `
+        
+          SELECT
+            t.id as type_id
+          FROM 
+            type t
+          where
+            t.id in (3,4)
+          ORDER BY rand() ASC 
+          LIMIT 1
+        
+      `;
+
+    return await this.createQueryBuilder()
+      .select(`concat(type1.type_id,',', type2.type_id ) as type_ids`)
+      .from('(' + fromTypeQuery1 + ')', 'type1')
+      .addFrom('(' + fromTypeQuery2 + ')', 'type2')
+      .getRawOne();
   }
 }
