643c14afbdbc590317c23d2b6eb42c545ea00c3d
wlgns1501 <wlgns1501@gmail.com>
2023-10-26 16:58:22 +0900

    20231026 helmet&csurf

--- src/lib/utils/exceptionFilter.ts
+++ src/lib/utils/exceptionFilter.ts
@@ -75,10 +75,10 @@ export class CustomExceptionFilter implements ExceptionFilter {
         - Time: ${body.timestamp}
         - detail
         - userId : ${req.user?.id}
-        - requestBody : ${Object?.values(request.body)[0] || ''}
-        - requestQuery : ${Object?.values(request.query)[0] || ''}
+        
         `;
-
+      // - requestBody : ${Object?.values(request.body)[0] || ''}
+      // - requestQuery : ${Object?.values(request.query)[0] || ''}
       try {
         await axios.post(webhookUrl, { content: errorMsg });
       } catch (e) {
